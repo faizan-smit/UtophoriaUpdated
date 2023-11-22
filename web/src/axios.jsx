a7f8d0838ace30635c5c1a912d30d4f3df30a766
Muhammd Faizan Alam <121306012+faizan-smit@users.noreply.github.com>
2023-11-23 00:46:06 +0500

    Some Errors Fixed 🛠️

--- web/src/axios.jsx
+++ web/src/axios.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: ('http://localhost:5001' || ""), // Your API base URL
-  // baseURL: '', // Your API base URL
+  // baseURL: "", // Your API base URL
+  baseURL: '', // Your API base URL
   withCredentials: true,
 });
 
